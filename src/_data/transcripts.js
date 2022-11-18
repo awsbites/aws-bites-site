2e86ac6b213e54c782d9449a23142ec302fa1695
Luciano Mammino <lucianomammino@gmail.com>
2022-11-18 17:37:32 +0000

    First draft of support for transcripts

---
+++ src/_data/transcripts.js
@@ -0,0 +1,18 @@
+const fs = require('fs/promises')
+const path = require('path')
+
+module.exports = async function transcripts () {
+  const transcriptsFolder = path.join(__dirname, '_transcripts')
+  const transcriptFiles = await fs.readdir(transcriptsFolder)
+
+  const transcripts = await Promise.all(transcriptFiles.map(async (filename) => {
+    const episodeNumber = filename.replace('.json', '')
+    const fileContent = JSON.parse(await fs.readFile(path.join(transcriptsFolder, filename), 'utf8'))
+
+    return [episodeNumber, fileContent]
+  }))
+
+  console.log(Object.fromEntries(transcripts))
+
+  return Object.fromEntries(transcripts)
+}
